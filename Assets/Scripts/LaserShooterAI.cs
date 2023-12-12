ed28e7db82d8a6adbdd8d04a1cd02762fd52775c
OmniVRS <scottkimball01@gmail.com>
2023-12-12 10:05:28 -0800

    Day 12 Complete

--- Assets/Scripts/LaserShooterAI.cs
+++ Assets/Scripts/LaserShooterAI.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class LaserShooterAI : MonoBehaviour
 {
+    private float downSpeed = 50;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(MoveDown());
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+
+    }
+
+    IEnumerator MoveDown()
+    {
+        while (transform.position.z > 50)
+        {
+            transform.Translate(Vector3.back * downSpeed * Time.deltaTime);
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
